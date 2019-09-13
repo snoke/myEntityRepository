3185a0e0fbc26343531ac52220f24917495f8616
unknown <stowwel@gmail.com>
2019-09-14 01:25:29 +0200

    Example added

---
+++ myEntityRepository/Example/Example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using myEntityRepository;
+using myEntityRepository.DataStorage;
+namespace myEntityRepositoryExample
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            List<Type> EntityTypes = new List<Type>() { Type.GetType("myEntityRepositoryExample.ExampleModel") };
+            EntityRepository Repo = new EntityRepository(EntityTypes, true);
+            ExampleModel obj = new ExampleModel("Joe Doe");
+            Repo.Save(obj);
+            Repo.Flush();
+        }
+    }
+}
---
+++ myEntityRepository/Example/ExampleModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using myEntityRepository.Model;
+
+namespace myEntityRepositoryExample
+{
+    public class ExampleModel:Entity
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public ExampleModel(string name) : base(null)
+        {
+            Name = name;
+        }
+        public ExampleModel(int? id, string name):base(id)
+        {
+            Name = name;
+        }
+
+    }
+}
